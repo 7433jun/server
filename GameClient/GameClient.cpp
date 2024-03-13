04759ced1027d4c3921df636b682187b40839d83
7433jun <7433jun@naver.com>
2024-03-13 15:02:18 +0900

    20240313

---
+++ GameClient/GameClient.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "Hello This is Client!\n";
+
+    while (true)
+    {
+        ;
+    }
+}
---
+++ GameServer/GameServer.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+#pragma commet(lib, "Ws2_32.lib")   // 라이브러리 불러오기
+#include <WinSock2.h>               // 소켓 서버 만들기 위해 필요
+
+int main()
+{
+    
+}
