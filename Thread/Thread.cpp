546064b082273cc86170cce7794e689f49c4e0b7
7433jun <7433jun@naver.com>
2024-03-25 15:03:02 +0900

    20240325

--- Thread/Thread.cpp
+++ Thread/Thread.cpp
@@ -1,30 +1,17 @@
 #include <iostream>
-#include <functional>
 
 using namespace std;
 
-class Obj
+void A()
 {
-public:
-	Obj() {cout << "积己" << endl; }
-	~Obj() { cout << "家戈" << endl; }
-public:
-	void Hello() { cout << "hello world" << endl; }
-};
+	cout << "A" << endl;
+}
 
 int main()
 {
-	std::shared_ptr<Obj> ptr1 = std::make_shared<Obj>();
-	cout << "Reference Count : " << ptr1.use_count() << endl;
+	A();
 
-	{
-		//笛促 啊瓷
-		//std::shared_ptr<Obj> ptr2 = std::move(ptr1);
-		std::shared_ptr<Obj> ptr2 = ptr1;
-		cout << "Reference Count : " << ptr1.use_count() << endl;
+	[]() {cout << "Lamda" << endl; }();
 
-	}//ptr2 家戈
-
-	cout << "Reference Count : " << ptr1.use_count() << endl;
 	return 0;
 }
