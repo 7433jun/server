97031caa6a1adbb288ccd4deeab71c7014fa0f26
7433jun <7433jun@naver.com>
2024-04-01 15:07:27 +0900

    20240401

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,57 +1,24 @@
 #include <iostream>
+#include <thread>
 using namespace std;
 
-#pragma comment(lib, "Ws2_32.lib")	
-#include <WinSock2.h>	
-#include <WS2tcpip.h> 
+#pragma comment(lib, "Ws2_32.lib")
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 
-#include <thread>
-#include <MSWSock.h>
-
-// Accept 함수 실행시킬 함수 포인터
-LPFN_ACCEPTEX lpfnAcceptEx = nullptr;
-// Accept용 GUID 설정
-GUID guidAcceptEx = WSAID_ACCEPTEX;
-
-SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0);
-
-
-
-// 비동기 I/O 작업을 처리하는 스레드 함수
-void AcceptThread(HANDLE iocpHandle)
+// IOCP를 사용하여 수신된 데이터를 처리하는 쓰레드 함수
+void RecvThread(HANDLE iocpHandle)
 {
-	DWORD bytesTransferred = 0;
-	ULONG_PTR key = 0;
-	WSAOVERLAPPED* overlapped = {};
-
 	while (true)
 	{
-		printf("Waiting...\n");
-		if (GetQueuedCompletionStatus(iocpHandle, &bytesTransferred, &key, (LPOVERLAPPED*)&overlapped, INFINITE))
-		{
-			printf("Client Connected\n");
+		printf("Hello\n");
 
-			char lpOutputBuf[1024];
-			SOCKET acceptSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-			DWORD dwBytes;
-
-			if (lpfnAcceptEx(listenSocket, acceptSocket, lpOutputBuf, 0, sizeof(SOCKADDR_IN) + 16, sizeof(SOCKADDR) + 16, &dwBytes, overlapped))
-			{
-				if (WSAGetLastError() != ERROR_IO_PENDING)
-				{
-					printf("AcceptEx failed with error : %d\n", WSAGetLastError());
-					closesocket(acceptSocket);
-					return 1;
-				}
-			}
-		}
+		this_thread::sleep_for(100ms);
 	}
 }
 
-
 int main()
 {
-
 	printf("==== SERVER ====\n");
 
 	WORD wVersionRequested;
@@ -59,13 +26,15 @@ int main()
 
 	wVersionRequested = MAKEWORD(2, 2);
 
+	// 생성
 	if (WSAStartup(wVersionRequested, &wsaData) != 0)
 	{
 		printf("WSAStartup failed with error\n");
 		return 1;
 	}
 
-	listenSocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
+	// 리슨소켓 생성
+	SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0);
 
 	if (listenSocket == INVALID_SOCKET)
 	{
@@ -74,12 +43,14 @@ int main()
 		return 1;
 	}
 
+	// 주소 생성
 	SOCKADDR_IN service;
 	memset(&service, 0, sizeof(service));
 	service.sin_family = AF_INET;
 	service.sin_addr.s_addr = htonl(INADDR_ANY);
 	service.sin_port = htons(27015);
 
+	// listenSocket에 주소 등록
 	if (bind(listenSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR)
 	{
 		printf("bind failed with error %d\n", WSAGetLastError());
@@ -88,6 +59,7 @@ int main()
 		return 1;
 	}
 
+	// 수신 대기
 	if (listen(listenSocket, 10) == SOCKET_ERROR)
 	{
 		printf("listen failed with error %d\n", WSAGetLastError());
@@ -96,76 +68,63 @@ int main()
 		return 1;
 	}
 
+
 	printf("listening...\n");
 
-	// AcceptEx 함수포인터 로드
-	DWORD dwBytes;
-	
-	// 함수 포인터에 값을 넣어줌
-	// WSAIoctl(소켓, 제어코드, 입력버퍼, 입력퍼버 크기, 출력버퍼 포인터, 출력버퍼 포인터 크기, 실제 출력 바이트 수, NULL, NULL);
-	// WSAIoctl(소켓, 제어코드, &GUID, GUID크기, accept함수포인터, LPFN_ACCEPTEX크기, 실제 출력 바이트 수, NULL, NULL);
-	if (WSAIoctl(listenSocket, SIO_GET_EXTENSION_FUNCTION_POINTER, &guidAcceptEx, sizeof(guidAcceptEx),
-		&lpfnAcceptEx, sizeof(lpfnAcceptEx), &dwBytes, NULL, NULL) == SOCKET_ERROR)
-	{
-		printf("WSAIoctl failed with error : %d", WSAGetLastError());
-		closesocket(listenSocket);
-		WSACleanup();
-		return 1;
-	}
-
-	// 클라이언트와 연결 수락할 소켓을 미리 생성
-	SOCKET acceptSocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
-	// 만든 소켓이 문제가 있다면
-	if (acceptSocket == INVALID_SOCKET)
-	{
-		printf("Accept Socket failedd with eror : %d\n", WSAGetLastError());
-		closesocket(listenSocket);
-		WSACleanup();
-		return 1;
-	}
-
-
-
-
-
-
-
-
-	// IOCP 생성
+	// IOCP 핸들 생성 및 초기화
 	HANDLE iocpHandle = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, NULL, NULL);
-	// 비동기 I/O 작업 처리를 위한 스레드 시작
-	thread t(AcceptThread, iocpHandle);
 
-	// key 설정
-	ULONG_PTR key = 0;
-	// listenSocket이기때문에 acceptSocket과 연결시켜주는것만 한다
-	// listenSocket을 IOCP에 연결
-	CreateIoCompletionPort((HANDLE)listenSocket, iocpHandle, key, 0);
+	// 스레드 만들어서 해당 함수 호출
+	// 매개변수로 CreateIoCompletionPort로 만들어진 새 핸들
+	thread t(RecvThread, iocpHandle);
 
-	char lpOutputBuf[1024];
-	// overlapped 초기화
-	//WSAOVERLAPPED overlapped;
-	//memset(&overlapped, 0, sizeof(overlapped));
-	WSAOVERLAPPED overlapped = {};
+	// 수신 데이터 버퍼
+	char recvBuffer[512] = {};
 
-	// 비동기 Accept
-	// lpfnAcceptEx(listen 소켓, accept 소켓, 설정값..., overlapped)
-	if (lpfnAcceptEx(listenSocket, acceptSocket, lpOutputBuf, 0, sizeof(SOCKADDR_IN) + 16, sizeof(SOCKADDR) + 16, &dwBytes, &overlapped))
+
+	while (true)
 	{
-		if (WSAGetLastError() != ERROR_IO_PENDING)
+		SOCKET acceptSocket = accept(listenSocket, NULL, NULL);
+		if (acceptSocket == INVALID_SOCKET)
 		{
-			printf("AcceptEx failed with error : %d\n", WSAGetLastError());
-			closesocket(acceptSocket);
+			closesocket(listenSocket); // 오류시 listenSocket 닫음
+			WSACleanup();
 			return 1;
 		}
+
+		printf("Client Connected\n"); // 클라이언트 연결 성공
+
+		// key 값은 0
+		ULONG_PTR key = 0;
+		// 클라이언트 소켓을 IOCP에 연결
+		CreateIoCompletionPort((HANDLE)acceptSocket, iocpHandle, key, 0);
+
+		// 수신 버퍼 설정
+		WSABUF wsaBuf;
+		wsaBuf.buf = recvBuffer;			// 받을 메모리 공간
+		wsaBuf.len = sizeof(recvBuffer);	// 받을 메모리 공간 크기
+
+		DWORD recvLen = 0;					// 수신된 데이터 길이를 저장할 변수
+		DWORD flags = 0;					// 플래그 변수, 현재는 사용하지 않음
+		WSAOVERLAPPED overlapped = {};		// WSAOVERLAPPED 구조체 할당후 초기화, 비동기 I/O 작업에 사용
+
+		// WSARecv
+		// s : 소켓. 위에 acceptSocket 넣어 주면됨
+		// lpBuffers : WSABUF 구조체 배열에 대한 포인터
+		// dwBufferCount : WSABUF 구조체 갯수
+		// lpNumberOfButesRecvd : 수신 완료 후 받은 바이트에 대한 포인터
+		// lpFlags : 플래그
+		// lpOverlapped : WSAOVERLAPPED 구조체 포인터
+		// lpCompletionRoutine : 수신 완료 후 호출되는 콜백함수
+		WSARecv(acceptSocket, OUT & wsaBuf, 1, OUT & recvLen, OUT & flags, &overlapped, NULL);
 	}
 
-	// TODo
-
-	// 스레드 종료 대기
+	// 스레드 완료까지 대기
 	t.join();
 
+	// 소켓 닫기
 	closesocket(listenSocket);
+	// 해제
 	WSACleanup();
 
 	return 0;
